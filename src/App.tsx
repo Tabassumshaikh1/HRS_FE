920eee15fdd8eadc3d36471c7aee8e49390b4fc7
Md. Amir <khanmdamir062@gmail.com>
2024-06-04 22:52:28 +0530

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "@mui/material";
+import Login from "./pages/Login";
+
+function App() {
+  return <Login />;
+}
+
+export default App;
